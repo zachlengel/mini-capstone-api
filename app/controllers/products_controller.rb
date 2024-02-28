99dc67b88ba1f72a6299ad477904b33600e64a97
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-02-28 14:43:35 -0500

    Added tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,4 +18,14 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @product = Product.create(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    render :show
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,26 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do
+    get "/products.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Product.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+    end
+  end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
+  end
 end
