cda2067e7ed6c754d01619a9fb4d511be5654933
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-03-22 14:35:59 -0400

    add order routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,17 @@
 class OrdersController < ApplicationController
+  def show
+    @order = Order.find_by(id: params["id"])
+    render :show
+  end
+
+  def index
+    @orders = Order.all
+    render :index
+  end
+
   def create
     @order = Order.create(
-      user_id: params["user_id"],
+      user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
       subtotal: params["subtotal"],
--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,8 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
 
   post "/sessions" => "sessions#create"
+
+  get "/orders/:id" => "orders#show"
+  get "/orders" => "orders#index"
+  post "/orders" => "orders#create"
 end
